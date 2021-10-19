d6694b344c71a09ff314605b6761a5076eef9cdd
WILLIAM DE SA RODRIGUES <wsr2k2@gmail.com>
2021-10-19 16:01:54 -0300

    Update index.js

--- model/database/index.js
+++ model/database/index.js
@@ -1,10 +1,16 @@
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize(process.env.DB_BASE, process.env.DB_USER,process.env.DB_PASS, {
-  host:  process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  dialect: 'postgres'
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: 'postgres',
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false 
+    }
+  },
+  ssl:true
 });
+
 
 async function conectado(){
     try {
